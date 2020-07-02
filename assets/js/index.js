9f12f519ab7e99ce25abc29b93f96a8676aebdd4
val8119 <mikk.vahaste@gmail.com>
2020-07-02 22:09:09 +0300

    Removed unused console.log() instances

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,16 +13,21 @@ function sendAPIRequest() {
     request.onload = function () {
         var data = JSON.parse(this.response);
 
-        console.log(data);
-
         longitude = data.longitude;
         latitude = data.latitude;
 
-        console.log(latitude, longitude)
+        let unix_timestamp = data.timestamp;
+
+        var date = new Date(unix_timestamp * 1000);
+        var hours = date.getHours();
+        var minutes = "0" + date.getMinutes();
+        var seconds = "0" + date.getSeconds();
+
+        var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 
         createMap(data.longitude, data.latitude);
 
-        containerP.innerText = `Longitude: ${data.longitude}\nLatitude ${data.latitude}`
+        containerP.innerText = `Longitude: ${data.longitude}\nLatitude ${data.latitude}\nLocal time: ${formattedTime}`
     }
 
     request.send();
